b11725e1b90274ebd6222f5ef718828cd8802255
Alexander D <alexandergdicks13@gmail.com>
2024-12-24 20:31:11 -0500

    Update programs.js

--- programs.js
+++ programs.js
@@ -1,18 +1,5 @@
 // File Comment Example
 
-// Search Bar
-
-function search() {
-    const input = document.getElementById('search-input').value;
-    const resultsContainer = document.getElementById('results');
-    
-    if (input) {
-        resultsContainer.innerHTML = `You searched for: ${input}`;
-    } else {
-        resultsContainer.innerHTML = 'Please enter a search term.';
-    }
-}
-
 // Navbar
 
 $(document).ready(function(){
